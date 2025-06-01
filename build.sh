1c1ce24cf43e8c6739e5fafb1b545e1fe2013ca0
RikkaRin <alukym0615@gmail.com>
2025-06-01 23:11:57 +0800

    Add --no-kmi-strict option to kernel build commands

--- build.sh
+++ build.sh
@@ -32,11 +32,11 @@ echo -e "===\n"
 cd android_kernel_xiaomi_marble
 mv ../build_kernel.sh .
 echo "[!] Build kernel image"
-bash build_kernel.sh Image
+bash build_kernel.sh --no-kmi-strict Image
 echo "[!] Build kernel image with ksu"
-bash build_kernel.sh --ksu Image
+bash build_kernel.sh --no-kmi-strict --ksu Image
 echo "[!] Build kernel modules"
-bash build_kernel.sh
+bash build_kernel.sh --no-kmi-strict
 cd ..
 
 echo "[!] Package kernel to flashable zip"
